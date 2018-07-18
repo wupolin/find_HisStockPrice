7b1f18228a49d1e6b34018505aea1d47b99b7f3c
wupolin <wupolin0102@gmail.com>
2018-07-18 15:31:57 +0800

    modified:   app.py 	deleted:    stock.py

--- app.py
+++ app.py
@@ -1,63 +1,55 @@
 from flask import Flask,jsonify
-import requests #step1
-from requests_html import HTML #step2
+import requests 
+from requests_html import HTML 
 
+def getlst(url,day):
+    resp = fetch(url)  #to fetch the website 
+    return print_stockinfo(int(day),resp.text) #return a list got from function print_stockinfo
 
-def fetch(url): #step1 : to fetch the website
+def fetch(url): #to fetch the website
     response = requests.get(url)
     return response
 
-def parse_article_entries(doc): #step2 : to find the entries we want in the website  
-    html = HTML(html=doc)
-    post_entries = html.find('table')
-    return post_entries
-
-
-def parse_article_meta(entry): #step3 : to catalog the data in the wedsite
+def parse_article_meta(entry): #to catalog the data from the wedsite
     return {
-    	'stocknum': entry.find('tr > th'), #find the stock number and name
-        'datatype': entry.find('thead tr td'), #find the datatype in the table
-        'data': entry.find('tbody tr td'), #find the data in the table
+    	'stocknum': entry.find('table tr > th'), #find the stock number and name
+        'datatype': entry.find('table thead tr td'), #find the datatype in the table
+        'data': entry.find('table tbody tr td'), #find the data in the table
     }
 
-def print_stockinfo(day,post_entries):
-    if len(post_entries)==0:
-        print('There isn\'t existing this stock symbol')
-    for entry in post_entries: 
-        meta = parse_article_meta(entry)
-        lst=[]
-        lst.append(meta['stocknum'][0].text[8:12])
-        print(meta['stocknum'][0].text[8:16]) #print STOCK NO. and name 
-        datatype=(meta['datatype'][0].text,meta['datatype'][3].text,meta['datatype'][4].text,meta['datatype'][5].text,meta['datatype'][6].text)
-        datalist=[]
-        i=0
-        while i<len(meta['data']):  #make data be a list cataloged by date, and store in datalist
-            tmplist=[]
-            tmplist.append(meta['data'][i].text)
-            j=1            
-            while j<9:
-                tmplist.append(meta['data'][i+j].text)                
-                j=j+1        
-            datalist.append(tmplist)
-            i=i+j               
-        datalist.reverse()  #make the latest data in the beginning of the datalist
-        i=0
-        while i<day:  #make the two row's data combine
-            if i==10:
-                print('The system is just storing the information of the stock in 10 days,\nso it\'s just shown the information in 10 days')
-                break
-            tmp=['Date : '+datalist[i][0],'Total Volume : '+datalist[i][1],'Open price : '+datalist[i][3],'High price : '+datalist[i][4],'Low price : '+datalist[i][5],'Close price : '+datalist[i][6]] 
-            lst.append(tmp)         
-            i=i+1
-        return lst
-def mainfunc(url,day):
-    resp = fetch(url)  # step-1
-    post_entries = parse_article_entries(resp.text)  # step-2
-    return print_stockinfo(int(day),post_entries)
+#arrange the stock data in the ideal order and store them in a list
+def print_stockinfo(day,pentries):
+    html = HTML(html=pentries)
+    meta = parse_article_meta(html)
+    lst=[]
+    lst.append(meta['stocknum'][0].text[8:12]) #put the stock symbol in the list
+    print(meta['stocknum'][0].text[8:16]) #print STOCK NO. and name on the terminal
+    datalist=[]
+    i=0
+    while i<len(meta['data']):  #make data be a list cataloged by date, and store in datalist
+        tmplist=[]
+        tmplist.append(meta['data'][i].text)
+        j=1            
+        while j<9:
+            tmplist.append(meta['data'][i+j].text)                
+            j=j+1        
+        datalist.append(tmplist)
+        i=i+j               
+    datalist.reverse()  #make the latest data in the beginning of the datalist
+    i=0
+    while i<day:  #make the two row's data combine
+        if i==10:
+            print('The system is just storing the information of the stock in 10 days,\nso it\'s just shown the information in 10 days')
+            break
+        tmp=['Date : '+datalist[i][0],'Total Volume : '+datalist[i][1],'Open price : '+datalist[i][3],'High price : '+datalist[i][4],'Low price : '+datalist[i][5],'Close price : '+datalist[i][6]] 
+        lst.append(tmp)         
+        i=i+1
+    return lst
 
+#web api :set up flask
 app=Flask(__name__)
 
-
+#web api : set the input path and type
 @app.route('/stock/id=<string:stocknum>&day=<string:day>',methods=['GET'])
 def get_tasks(stocknum,day):
     #check if input data is correct 
@@ -68,7 +60,7 @@ def get_tasks(stocknum,day):
         stock=[{'Error' : 'please input the type of Integer (e.g. day=5)'}]
         return jsonify(stock)
     url = 'http://www.twse.com.tw/exchangeReport/STOCK_DAY?response=html&date=20180715&stockNo='+stocknum
-    lst=mainfunc(url,day)
+    lst=getlst(url,day)
     #if the stock symbol isn't existing
     if lst==None:
         stock=[{'Error' : 'There isn\'t existing this stock symbol'}]
@@ -82,6 +74,7 @@ def get_tasks(stocknum,day):
     while i<len(lst):			
         stock.append(lst[i])
         i=i+1
+    #if requested to output over 10 days, then output the reason that we can just output 10 days 
     if int(day)>10:
         stock.append('Notice:')
         stock.append('There are just shown the information in 10 days')
